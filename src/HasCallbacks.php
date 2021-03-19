b45c85d55332ebedc778b3fb7e37e643babe0ee6
Filip Ganyicz <ganyicz.filip@gmail.com>
2021-03-19 19:06:31 +0100

    Add HasCallbacks trait

---
+++ src/HasCallbacks.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Ganyicz\NovaCallbacks;
+
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+trait HasCallbacks
+{
+    public static function fill(NovaRequest $request, $model)
+    {
+        if (method_exists(static::class, 'afterSave')) {
+            $model::saved(function ($model) use ($request) {
+                static::afterSave($request, $model);
+            });
+        }
+
+        if (method_exists(static::class, 'afterCreate')) {
+            $model::saved(function ($model) use ($request) {
+                static::afterCreate($request, $model);
+            });
+        }
+        
+        return static::fillFields(
+            $request, $model,
+            (new static($model))->creationFieldsWithoutReadonly($request)
+        );
+    }
+
+    public static function fillForUpdate(NovaRequest $request, $model)
+    {
+        if (method_exists(static::class, 'afterSave')) {
+            $model::saved(function ($model) use ($request) {
+                static::afterSave($request, $model);
+            });
+        }
+        
+        if (method_exists(static::class, 'afterUpdate')) {
+            $model::saved(function ($model) use ($request) {
+                static::afterUpdate($request, $model);
+            });
+        }
+
+        return static::fillFields(
+            $request, $model,
+            (new static($model))->updateFieldsWithoutReadonly($request)
+        );
+    }
+}
