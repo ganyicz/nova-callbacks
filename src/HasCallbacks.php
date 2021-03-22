e88ba488fc79e1a6db50a098681b6b32a1404e30
Filip Ganyicz <ganyicz.filip@gmail.com>
2021-03-22 11:42:11 +0100

    Add before callbacks

--- src/HasCallbacks.php
+++ src/HasCallbacks.php
@@ -8,6 +8,14 @@ trait HasCallbacks
 {
     public static function fill(NovaRequest $request, $model)
     {
+        if (method_exists(static::class, 'beforeSave')) {
+            static::beforeSave($request, $model);
+        }
+
+        if (method_exists(static::class, 'beforeCreate')) {
+            static::beforeCreate($request, $model);
+        }
+
         if (method_exists(static::class, 'afterSave')) {
             $model::saved(function ($model) use ($request) {
                 static::afterSave($request, $model);
@@ -28,6 +36,14 @@ trait HasCallbacks
 
     public static function fillForUpdate(NovaRequest $request, $model)
     {
+        if (method_exists(static::class, 'beforeSave')) {
+            static::beforeSave($request, $model);
+        }
+
+        if (method_exists(static::class, 'beforeUpdate')) {
+            static::beforeUpdate($request, $model);
+        }
+
         if (method_exists(static::class, 'afterSave')) {
             $model::saved(function ($model) use ($request) {
                 static::afterSave($request, $model);
